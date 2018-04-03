c7843cbb07d5dcdbdd110dda021d3154db58404f
Lukasz Wroblewski OS <hawkers@op.pl>
2018-04-03 15:23:04 +0200

    correct FilteredQuizListFragment, basic

--- app/src/main/java/pl/iosx/quiz4wp/ui/category/filteredquizlist/FilteredQuizListFragment.java
+++ app/src/main/java/pl/iosx/quiz4wp/ui/category/filteredquizlist/FilteredQuizListFragment.java
@@ -1,9 +1,17 @@
 package pl.iosx.quiz4wp.ui.category.filteredquizlist;
 
+import android.view.View;
+
+import pl.iosx.quiz4wp.ui.base.BaseFragment;
+
 /**
  * Created by lukaszwroblewski on 03.04.2018.
  */
 
-public class FilteredQuizListFragment {
+public class FilteredQuizListFragment<V extends FilteredQuizListMvpView> extends BaseFragment{
 
+    @Override
+    protected void setUp(View view) {
+
+    }
 }
